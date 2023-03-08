25653e7bff935db63d9a0afba63787258939072e
Sergeeva Evgenia <evgenia.serrgeeva@gmail.com>
2023-03-08 16:04:35 +0200

    react introduction

---
+++ blog/src/components/Banner/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css';
+
+const Banner = () => {
+    return <div className="banner">
+        <p>Full-width banner image</p>
+    </div>
+};
+
+export default Banner;
---
+++ forecast/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Forecast from "./pages/Forecast";
+
+function App() {
+  return (
+    <div className="container">
+      <Forecast/>
+    </div>
+  );
+}
+
+export default App;
